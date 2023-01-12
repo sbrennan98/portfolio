b59e679ca081046a5e7fb18386f7b7089b5c03e8
Sean Brennan <slbvp6@mail.missouri.edu>
2023-01-12 16:58:32 -0600

    feat(inspire): added vue reactvity & directives

---
+++ components/AlbertEinstein.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-col class="text-center">
+    <img
+      src="https://1.bp.blogspot.com/-eeYOiXwnehw/X3Rvi4Od0RI/AAAAAAAAA4U/94PRVjVnyKUE_R5Z5B05_IJqiDK2gnA2ACLcBGAsYHQ/s1920/Albert%2BEinstein%2Bneelcrew.jpg"
+      alt="Albert Einstein"
+      class="mb-2"
+    >
+    <blockquote class="blockquote">
+      &#8220;Computers are incredibly fast, accurate, and stupid. Human beings are incredibly slow, inaccurate, and brilliant. Together they are powerful beyond imagination.&#8221;
+      <footer>
+        <small>
+          <em>&mdash;Albert Einstein</em>
+        </small>
+      </footer>
+    </blockquote>
+  </v-col>
+</template>
+
+<style scoped>
+img {
+    width:100%;
+    max-width:400px;
+}
+</style>
