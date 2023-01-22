c07c6edbda4189555cf459647ba16de30daa751a
Sean Lee Brennan <brennansean6@gmail.com>
2023-01-22 16:05:38 -0600

    fix(nav): better nav drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,6 @@
     <v-app theme="dark">
       <v-navigation-drawer
         v-model="drawer"
-        :mini-variant="miniVariant"
-        :clipped="clipped"
         fixed
         app
       >
@@ -14,32 +12,16 @@
             :to="item.to"
             router
             exact
-          >
-            <v-list-item-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-action>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item>
+            :prepend-icon="item.icon"
+            :title="item.title"
+          />
         </v-list>
       </v-navigation-drawer>
       <v-app-bar
-        :clipped-left="clipped"
         fixed
         app
       >
         <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-        <v-btn
-          icon
-          @click.stop="miniVariant = !miniVariant"
-        >
-          <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-        </v-btn>
-        <v-btn
-          icon
-          @click.stop="clipped = !clipped"
-        >
-          <v-icon>mdi-application</v-icon>
-        </v-btn>
         <v-toolbar-title v-text="title" />
       </v-app-bar>
       <v-main>
@@ -61,7 +43,6 @@
     name: 'DefaultLayout',
     data () {
       return {
-        clipped: false,
         drawer: false,
         fixed: true,
         items: [
@@ -86,7 +67,6 @@
             to: '/sean/projects'
           }
         ],
-        miniVariant: false,
         right: true,
         rightDrawer: false,
         title: "Sean's Portfolio"
